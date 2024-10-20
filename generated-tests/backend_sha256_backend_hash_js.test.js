074108d6014471c4d65899d0a33364a03c39fa1c
Katerina Paltoglou <katpaltoglou@gmail.com>
2024-10-20 15:24:46 +0300

    added tests

---
+++ generated-tests/backend_sha256_backend_hash_js.test.js
@@ -0,0 +1,225 @@
+describe(
+  "../evaluationProjects/bcoin/lib/crypto/backend-hash.js:14:16:16:1",
+  () => {
+    test("invoc-loc:43:13:43:42-test:0", () => {
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/lib/crypto/backend.js:16:1:16:44
+      var sha256Obj = require("../lib/crypto/backend-hash.js");
+      var arg0 = "sha256";
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/filtered-test-modules/bcoin-test-code-merged-entryfile.js:128:5:128:32
+      var invArg0 = new Buffer([]);
+      var invArg0;
+      var data;
+      var arg1 = invArg0;
+      var actualResult = sha256Obj.hash(arg0, arg1);
+      expect(actualResult["0"]).toBe(227);
+      expect(actualResult["1"]).toBe(176);
+      expect(actualResult["2"]).toBe(196);
+      expect(actualResult["3"]).toBe(66);
+      expect(actualResult["4"]).toBe(152);
+      expect(actualResult["5"]).toBe(252);
+      expect(actualResult["6"]).toBe(28);
+      expect(actualResult["7"]).toBe(20);
+      expect(actualResult["8"]).toBe(154);
+      expect(actualResult["9"]).toBe(251);
+      expect(actualResult["10"]).toBe(244);
+      expect(actualResult["11"]).toBe(200);
+      expect(actualResult["12"]).toBe(153);
+      expect(actualResult["13"]).toBe(111);
+      expect(actualResult["14"]).toBe(185);
+      expect(actualResult["15"]).toBe(36);
+      expect(actualResult["16"]).toBe(39);
+      expect(actualResult["17"]).toBe(174);
+      expect(actualResult["18"]).toBe(65);
+      expect(actualResult["19"]).toBe(228);
+      expect(actualResult["20"]).toBe(100);
+      expect(actualResult["21"]).toBe(155);
+      expect(actualResult["22"]).toBe(147);
+      expect(actualResult["23"]).toBe(76);
+      expect(actualResult["24"]).toBe(164);
+      expect(actualResult["25"]).toBe(149);
+      expect(actualResult["26"]).toBe(153);
+      expect(actualResult["27"]).toBe(27);
+      expect(actualResult["28"]).toBe(120);
+      expect(actualResult["29"]).toBe(82);
+      expect(actualResult["30"]).toBe(184);
+      expect(actualResult["31"]).toBe(85);
+    });
+
+    test("invoc-loc:43:13:43:42-test:1", () => {
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/lib/crypto/backend.js:16:1:16:44
+      var sha256Obj = require("../lib/crypto/backend-hash.js");
+      var arg0 = "sha256";
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/filtered-test-modules/bcoin-test-code-merged-entryfile.js:133:5:133:32
+      var invArg1 = new Buffer([]);
+      var invArg1;
+      var data;
+      var arg1 = invArg1;
+      var actualResult = sha256Obj.hash(arg0, arg1);
+      expect(actualResult["0"]).toBe(227);
+      expect(actualResult["1"]).toBe(176);
+      expect(actualResult["2"]).toBe(196);
+      expect(actualResult["3"]).toBe(66);
+      expect(actualResult["4"]).toBe(152);
+      expect(actualResult["5"]).toBe(252);
+      expect(actualResult["6"]).toBe(28);
+      expect(actualResult["7"]).toBe(20);
+      expect(actualResult["8"]).toBe(154);
+      expect(actualResult["9"]).toBe(251);
+      expect(actualResult["10"]).toBe(244);
+      expect(actualResult["11"]).toBe(200);
+      expect(actualResult["12"]).toBe(153);
+      expect(actualResult["13"]).toBe(111);
+      expect(actualResult["14"]).toBe(185);
+      expect(actualResult["15"]).toBe(36);
+      expect(actualResult["16"]).toBe(39);
+      expect(actualResult["17"]).toBe(174);
+      expect(actualResult["18"]).toBe(65);
+      expect(actualResult["19"]).toBe(228);
+      expect(actualResult["20"]).toBe(100);
+      expect(actualResult["21"]).toBe(155);
+      expect(actualResult["22"]).toBe(147);
+      expect(actualResult["23"]).toBe(76);
+      expect(actualResult["24"]).toBe(164);
+      expect(actualResult["25"]).toBe(149);
+      expect(actualResult["26"]).toBe(153);
+      expect(actualResult["27"]).toBe(27);
+      expect(actualResult["28"]).toBe(120);
+      expect(actualResult["29"]).toBe(82);
+      expect(actualResult["30"]).toBe(184);
+      expect(actualResult["31"]).toBe(85);
+    });
+
+    test("invoc-loc:43:13:43:42-test:2", () => {
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/lib/crypto/backend.js:16:1:16:44
+      var sha256Obj = require("../lib/crypto/backend-hash.js");
+      var arg0 = "sha256";
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/filtered-test-modules/bcoin-test-code-merged-entryfile.js:138:5:138:32
+      var invArg2 = new Buffer([]);
+      var invArg2;
+      var data;
+      var arg1 = invArg2;
+      var actualResult = sha256Obj.hash(arg0, arg1);
+      expect(actualResult["0"]).toBe(227);
+      expect(actualResult["1"]).toBe(176);
+      expect(actualResult["2"]).toBe(196);
+      expect(actualResult["3"]).toBe(66);
+      expect(actualResult["4"]).toBe(152);
+      expect(actualResult["5"]).toBe(252);
+      expect(actualResult["6"]).toBe(28);
+      expect(actualResult["7"]).toBe(20);
+      expect(actualResult["8"]).toBe(154);
+      expect(actualResult["9"]).toBe(251);
+      expect(actualResult["10"]).toBe(244);
+      expect(actualResult["11"]).toBe(200);
+      expect(actualResult["12"]).toBe(153);
+      expect(actualResult["13"]).toBe(111);
+      expect(actualResult["14"]).toBe(185);
+      expect(actualResult["15"]).toBe(36);
+      expect(actualResult["16"]).toBe(39);
+      expect(actualResult["17"]).toBe(174);
+      expect(actualResult["18"]).toBe(65);
+      expect(actualResult["19"]).toBe(228);
+      expect(actualResult["20"]).toBe(100);
+      expect(actualResult["21"]).toBe(155);
+      expect(actualResult["22"]).toBe(147);
+      expect(actualResult["23"]).toBe(76);
+      expect(actualResult["24"]).toBe(164);
+      expect(actualResult["25"]).toBe(149);
+      expect(actualResult["26"]).toBe(153);
+      expect(actualResult["27"]).toBe(27);
+      expect(actualResult["28"]).toBe(120);
+      expect(actualResult["29"]).toBe(82);
+      expect(actualResult["30"]).toBe(184);
+      expect(actualResult["31"]).toBe(85);
+    });
+
+    test("invoc-loc:43:13:43:42-test:3", () => {
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/lib/crypto/backend.js:16:1:16:44
+      var sha256Obj = require("../lib/crypto/backend-hash.js");
+      var arg0 = "sha256";
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/filtered-test-modules/bcoin-test-code-merged-entryfile.js:151:5:151:33
+      var invArg00 = new Buffer([]);
+      var invArg00;
+      var data;
+      var arg1 = invArg00;
+      var actualResult = sha256Obj.hash(arg0, arg1);
+      expect(actualResult["0"]).toBe(227);
+      expect(actualResult["1"]).toBe(176);
+      expect(actualResult["2"]).toBe(196);
+      expect(actualResult["3"]).toBe(66);
+      expect(actualResult["4"]).toBe(152);
+      expect(actualResult["5"]).toBe(252);
+      expect(actualResult["6"]).toBe(28);
+      expect(actualResult["7"]).toBe(20);
+      expect(actualResult["8"]).toBe(154);
+      expect(actualResult["9"]).toBe(251);
+      expect(actualResult["10"]).toBe(244);
+      expect(actualResult["11"]).toBe(200);
+      expect(actualResult["12"]).toBe(153);
+      expect(actualResult["13"]).toBe(111);
+      expect(actualResult["14"]).toBe(185);
+      expect(actualResult["15"]).toBe(36);
+      expect(actualResult["16"]).toBe(39);
+      expect(actualResult["17"]).toBe(174);
+      expect(actualResult["18"]).toBe(65);
+      expect(actualResult["19"]).toBe(228);
+      expect(actualResult["20"]).toBe(100);
+      expect(actualResult["21"]).toBe(155);
+      expect(actualResult["22"]).toBe(147);
+      expect(actualResult["23"]).toBe(76);
+      expect(actualResult["24"]).toBe(164);
+      expect(actualResult["25"]).toBe(149);
+      expect(actualResult["26"]).toBe(153);
+      expect(actualResult["27"]).toBe(27);
+      expect(actualResult["28"]).toBe(120);
+      expect(actualResult["29"]).toBe(82);
+      expect(actualResult["30"]).toBe(184);
+      expect(actualResult["31"]).toBe(85);
+    });
+
+    test("invoc-loc:43:13:43:42-test:4", () => {
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/lib/crypto/backend.js:16:1:16:44
+      var sha256Obj = require("../lib/crypto/backend-hash.js");
+      var arg0 = "sha256";
+      ///home/katerina/visualStudioGit/evaluationProjects/bcoin/filtered-test-modules/bcoin-test-code-merged-entryfile.js:157:5:157:33
+      var invArg10 = new Buffer([]);
+      var invArg10;
+      var data;
+      var arg1 = invArg10;
+      var actualResult = sha256Obj.hash(arg0, arg1);
+      expect(actualResult["0"]).toBe(227);
+      expect(actualResult["1"]).toBe(176);
+      expect(actualResult["2"]).toBe(196);
+      expect(actualResult["3"]).toBe(66);
+      expect(actualResult["4"]).toBe(152);
+      expect(actualResult["5"]).toBe(252);
+      expect(actualResult["6"]).toBe(28);
+      expect(actualResult["7"]).toBe(20);
+      expect(actualResult["8"]).toBe(154);
+      expect(actualResult["9"]).toBe(251);
+      expect(actualResult["10"]).toBe(244);
+      expect(actualResult["11"]).toBe(200);
+      expect(actualResult["12"]).toBe(153);
+      expect(actualResult["13"]).toBe(111);
+      expect(actualResult["14"]).toBe(185);
+      expect(actualResult["15"]).toBe(36);
+      expect(actualResult["16"]).toBe(39);
+      expect(actualResult["17"]).toBe(174);
+      expect(actualResult["18"]).toBe(65);
+      expect(actualResult["19"]).toBe(228);
+      expect(actualResult["20"]).toBe(100);
+      expect(actualResult["21"]).toBe(155);
+      expect(actualResult["22"]).toBe(147);
+      expect(actualResult["23"]).toBe(76);
+      expect(actualResult["24"]).toBe(164);
+      expect(actualResult["25"]).toBe(149);
+      expect(actualResult["26"]).toBe(153);
+      expect(actualResult["27"]).toBe(27);
+      expect(actualResult["28"]).toBe(120);
+      expect(actualResult["29"]).toBe(82);
+      expect(actualResult["30"]).toBe(184);
+      expect(actualResult["31"]).toBe(85);
+    });
+  }
+);
+
